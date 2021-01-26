bc8e5f443ac514237407a7bb07de4cf0a4c8acea
Stephan Wehrheim <swehrheim@geospin.de>
2021-01-26 15:14:01 +0100

    Add takel-systemd

---
+++ ansible/roles/takel-systemd/molecule/default/system/test_takel-systemd_system_services.py
@@ -0,0 +1,28 @@
+import takeltest
+import pytest
+
+testinfra_hosts = takeltest.hosts()
+
+
+#@pytest.mark.xfail
+def test_takel_systemd_services_service_enabled(host, testvars):
+    systemd_services_list = testvars['takel_systemd_service_list']
+    systemd_service_enabled = \
+        host.check_output('systemctl list-unit-files | grep enabled')
+    for service in systemd_services_list:
+        assert service['name'] in systemd_service_enabled
+
+
+# #@pytest.mark.xfail
+# def test_takel_systemd_services_service_running(host, testvars):
+#     systemd_services_list = testvars['takel_systemd_service_list']
+#     for service in systemd_services_list:
+#         assert host.service(service['name'] + '.service').is_running
+
+
+def test_takel_systemd_services_service_running(host, testvars):
+    systemd_services_list = testvars['takel_systemd_service_list']
+    systemd_service_enabled = \
+        host.check_output('systemctl list-unit-files | grep enabled')
+    for service in systemd_services_list:
+        assert service['name'] in systemd_service_enabled
